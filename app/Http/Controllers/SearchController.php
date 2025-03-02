aafeb43b44644c1cba0e189f5ccd1566e6a15b9e
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-02 22:42:09 +0600

    Add SearchController for blog content search functionality

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function show(Request $request)
+    {
+        $query = $request->input('query');
+        $posts = Post::where('title', 'like', '%' . $query . '%')
+            ->orWhere('content', 'like', '%' . $query . '%')
+            ->get();
+        return view('search.results', compact('query', 'posts'));
+    }
+}
