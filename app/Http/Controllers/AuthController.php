217475688f6376bea0b11156874e83a0597570ac
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-02 22:41:27 +0600

    Add AuthController for user authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        // Authentication logic
+    }
+
+    public function logout()
+    {
+        // Logout logic
+    }
+}
