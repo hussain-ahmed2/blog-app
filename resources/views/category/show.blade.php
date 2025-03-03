1c48c6d2e8c59bfdb70223b1bbfaa1031ce659b8
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-04 00:58:37 +0600

    add posts based on the particular category

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,6 +1,16 @@
 <x-layout>
     <x-slot:pageTitle>Blog Category - {{ $category->name }}</x-slot:pageTitle>
     
-    <x-page-heading name="{{ $category->name }}" />
+    <x-heading name="{{ $category->name }}" />
+
+    @if ($posts->count())
+        <x-post.post-container :$posts />
+
+        <div class="mt-5">
+            {{ $posts->links('pagination::custom') }}
+        </div>
+    @else
+        <p class="text-slate-600">No Post available.</p>
+    @endif
 
 </x-layout>
