83a2636e5390e77f87aecdcd017ab0a239c5da78
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-04 00:57:10 +0600

    add category grid links

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,5 +1,11 @@
 <x-layout>
     <x-slot:pageTitle>Categories</x-slot:pageTitle>
-    
+
     <x-heading name="Categories" />
+
+    <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5">
+        @foreach ($categories as $category)
+            <a class="btn border-slate-300" href="/categories/{{ $category->id }}">{{ $category->name }}</a>
+        @endforeach
+    </div>
 </x-layout>
