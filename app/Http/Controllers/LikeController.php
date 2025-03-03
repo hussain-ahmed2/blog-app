2f975d03efcc8c4abbfd029c3edbf616da146efc
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-03 15:04:39 +0600

    Improve like controller implementation

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Like;
-use App\Http\Requests\StoreLikeRequest;
-use App\Http\Requests\UpdateLikeRequest;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
     public function toggleLike($id)
     {
-        $like = Like::where('post_id', $id)->where('user_id', auth()->id)->first();
+        $like = Like::where('post_id', $id)->where('user_id', Auth::id())->first();
+
         if ($like) {
             $like->delete();
         } else {
-            Like::create(['post_id' => $id, 'user_id' => auth()->id]);
+            Like::create(['post_id' => $id, 'user_id' => Auth::id()]);
         }
         return back();
     }
