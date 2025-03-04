4e9b83fcf51ad954833b2b07a7bf95f6dd6c61d2
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-05 01:09:45 +0600

    style: update hero section color scheme to cyan for consistency

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,11 @@
     <x-slot:pageTitle>Blog</x-slot:pageTitle>
 
     <section class="">
-        <div class="bg-slate-600 text-white py-16 text-center rounded-sm">
+        <div class="bg-cyan-600 text-white py-16 text-center rounded-sm">
             <h1 class="text-4xl font-bold">Welcome to Blog</h1>
             <p class="mt-2 text-lg">Explore articles, tutorials, and tech insights.</p>
             <a href="/posts"
-                class="mt-4 inline-block bg-white text-slate-600 font-semibold px-6 py-2 rounded-full hover:bg-gray-200">
+                class="mt-4 inline-block bg-white text-cyan-600 font-semibold px-6 py-2 rounded-full hover:bg-gray-200">
                 View Posts
             </a>
         </div>
