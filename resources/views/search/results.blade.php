1114059cd68b049d29ec25c384d5db2dadba6f0c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-05 01:09:54 +0600

    feat: enhance search results page with search form and pagination

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -3,7 +3,20 @@
 
     <x-heading name="Search" />
 
-    <p>
-        Search result for: {{ $query }}
-    </p>
+    <form method="GET" action="/search" class="flex gap-1 mb-5">
+        <input class="input bg-white" type="text" name="query" placeholder="type here..." value="{{ request('query')}}">
+        <x-form.form-button class="border-slate-300 bg-cyan-500 hover:bg-cyan-400">Search</x-form.form-button>
+    </form>
+
+    @if ($query)
+        <p class="mb-5">
+            Search result for: {{ $query }}
+        </p>
+
+        <x-post.post-container :$posts />
+
+        <div class="mt-5">
+            {{ $posts->links('pagination::custom') }}
+        </div>
+    @endif
 </x-layout>
