e99527239ba6b09155378e4dac872948b1dd5844
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-05 01:09:35 +0600

    feat: add active state indicators to navigation links

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,16 +2,16 @@
     <a class="logo" href="/">Blog</a>
 
     <div class="link-group">
-        <a class="link" href="/">Home</a>
-        <a class="link" href="/posts">Posts</a>
-        <a class="link" href="/categories">Categories</a>
-        <a class="link" href="/search">Search</a>
+        <a class="link {{ Request::is('/') ? "text-cyan-500" : ""}}" href="/">Home</a>
+        <a class="link {{ Request::is('posts') ? "text-cyan-500" : ""}}" href="/posts">Posts</a>
+        <a class="link {{ Request::is('categories') ? "text-cyan-500" : ""}}" href="/categories">Categories</a>
+        <a class="link {{ Request::is('search') ? "text-cyan-500" : ""}}" href="/search">Search</a>
     </div>
 
     @auth
         <div class="link-group">
-            <a class="link" href="/posts/create">Create</a>
-            <a class="link" href="/dashboard">Dashboard</a>
+            <a class="link {{ Request::is('posts/create') ? "text-cyan-500" : ""}}" href="/posts/create">Create</a>
+            <a class="link {{ Request::is('dashboard') ? "text-cyan-500" : ""}}" href="/dashboard">Dashboard</a>
             <form method="POST" action="/logout">
                 @csrf
                 @method("DELETE")
@@ -21,8 +21,8 @@
         </div>
     @else
         <div class="link-group">
-            <a class="btn hover:border-slate-300" href="/login">Login</a>
-            <a class="btn bg-slate-200 hover:bg-slate-300" href="/register">Register</a>
+            <a class="btn hover:border-slate-300 {{ Request::is('login') ? "text-cyan-500" : ""}}" href="/login">Login</a>
+            <a class="btn {{ Request::is('register') ? "bg-cyan-500 text-white" : "bg-slate-200 hover:bg-slate-300"}}" href="/register">Register</a>
         </div>
     @endauth
 </nav>
